ca3e057b0e39c4831866ca6e68631b193ca07da0
xLong <381612175@qq.com>
2018-02-09 09:01:39 +0800

    解决了：“首页”通知轮播问题

--- src/pages/Index/Index.vue
+++ src/pages/Index/Index.vue
@@ -1,28 +1,22 @@
 <template>
 	<div id="index">
-		<!-- 消息通知 -->
 		<section class="notice_part cont_frame">
-			<span class="notice_icon fl"><i class="icon-horn fl"></i>通知：</span>
-			<!-- 消息轮播 -->
-			<swiper :options="swiperOption" ref="mySwiper" class="notice_list">
-				<swiper-slide v-for="(item, index) in noticeList" :key="index">        
-					{{ item }}
-				</swiper-slide>
-			</swiper>
+			<span class="notice_icon fl"><i class="icon-horn"></i>通知：</span>
+			<div id="scroll_list" class="notice_list" >
+				<div id="scroll_begin">{{ noticeTxt }}</div>
+				<div id="scroll_end"></div>
+			</div>
 		</section>
-		<!-- 申报咨询 -->
 		<section class="news_part">
 			<SectionTitle title="申报资讯" :read-more="true" route-name="ProjectNews"></SectionTitle>
 			<NewsList :data="newsList"></NewsList>
 		</section>
-		<!-- 成功案例 -->
 		<section class="case_part">
-			<SectionTitle title="成功案例" :read-more="true" route-name="ProjectNews"></SectionTitle>
+			<SectionTitle title="成功案例" :read-more="true" route-name="SuccCases"></SectionTitle>
 			<div class="cont_frame">内容</div>
 		</section>
-		<!-- 合作伙伴 -->
 		<section class="cooper_part">
-			<SectionTitle title="合作伙伴" :read-more="true" route-name="ProjectNews"></SectionTitle>
+			<SectionTitle title="合作伙伴" :read-more="true"></SectionTitle>
 			<div class="cont_frame">内容</div>
 		</section>
 	</div>
@@ -34,15 +28,12 @@
 	import SectionTitle from "components/Common/SectionTitle.vue";
 	import NewsList from "components/News/NewsList.vue";
 
+	// 定时器
+	var noticeScroll;
+
 	export default {
 		name: "index",
 		components: { SectionTitle, NewsList },
-		computed: {
-			swiper() {
-				console.log(this.$refs.mySwiper.swiper);
-				return this.$refs.mySwiper.swiper;  
-			}  
-		},
 		data() {
 			return {
 				newsList: [
@@ -92,55 +83,90 @@
 					'恭喜xLong成功申请了牛逼轰轰的项目！！',
 					'恭喜Lio.Huang成功申请了牛逼轰轰的项目！！'
 				],
-				// vue-awesome-swiper 参数及使用方法请查看官方说明 https://surmon-china.github.io/vue-awesome-swiper
-				swiperOption: {   
-					slidesPerView: 'auto',
-					spaceBetween: 0,
-					speed: 1000,
-					autoplay: true,
-					loop: true,             
-				}
+				noticeTxt: '',
 			}
 		},
 		created(){
+			this.setNoticeTxt();
 		},
 		mounted(){
+			this.noticeMove();
 		},
 		methods:{
+			// 设置通知内容
+			setNoticeTxt(){
+				if(this.noticeList.length > 0){
+					for(let i = 0; i < this.noticeList.length; i++){
+						this.noticeTxt += this.noticeList[i];
+					}
+				}
+			},
+			// 通知移动
+			noticeMove(){
+				var speed = 50;
+				var scroll_begin = document.getElementById("scroll_begin");
+				var scroll_end = document.getElementById("scroll_end");
+				var scroll_list = document.getElementById("scroll_list");
+				scroll_end.innerHTML = scroll_begin.innerHTML;
+				noticeScroll = setInterval(function(){
+					if (scroll_end.offsetWidth - scroll_list.scrollLeft <= 0)
+						scroll_list.scrollLeft -= scroll_begin.offsetWidth;
+					else
+						scroll_list.scrollLeft++;
+
+					console.log(scroll_list.scrollLeft);
+				}, speed);
+			}
 		}
 	};
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
 	// 引入通用设置文件
 	@import "../../assets/less/setting";
 
+	#gongao {
+        width: 1000px;
+        height: 30px;
+        overflow: hidden;
+        line-height: 30px;
+        font-size: 13px;
+        font-family: '宋体';
+        background: #DDE5ED;
+        color: #0C77CF;
+        font-weight: bold;
+    }
+
+    #gongao #scroll_begin,
+    #gongao #scroll_end {
+        display: inline
+    }
+
 	.notice_part{
-		// .swiper-slide{
-		// 	width: auto;
-		// }
 	}
 
 	.notice_icon{
 		color: @base_color;
 
 		i{
-			.mt(2);
 			.mr(5);
-			.ft(16);
 		}
 	}
 
-	.notice_list{
-		overflow: hidden;
+	.notice_list{		
+		width: 79%;
 		.ht(24);
 
-		.n_li_item {
-		}
-		
+		.ellipsis;		
 	}
 
 	.news_part{
 		.mb(10);
+	}
+
+	@media screen and (min-width: 414px) {
+		.notice_list{
+			width: 83%;
+		}
 	}
 </style>
