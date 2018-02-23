c5398fae518658694f7e10085b6999c13e12d798
xLong <381612175@qq.com>
2018-02-23 17:02:18 +0800

    新增：“资讯列表”页滚动到底部无限加载数据功能和“返回顶部”按钮

---
+++ src/components/Common/BackTop.vue
@@ -0,0 +1,38 @@
+<template>
+	<a id="backTop" class="back_top" @click="goBackTop">返回顶部</a>
+</template>
+
+<script>
+
+	export default {
+		name: "backTop",
+		data() {
+			return {}
+		},
+		methods:{
+            // 返回顶部
+            goBackTop(){
+                document.body.scrollTop = 0;
+                document.documentElement.scrollTop = 0;
+            }
+		}
+	};
+</script>
+
+<style lang="less" scoped>
+    // 引入通用设置文件
+    @import "../../assets/less/setting";
+
+    .back_top{
+        position: fixed;
+        background: lighten(@base_color, 10%);
+        padding: 8*@rem;
+        color: #fff;
+        z-index: 99;
+        bottom: 50*@rem;
+        right: @edge_w;
+
+        .ft(12);
+        .border_radius_4;
+    }
+</style>
--- src/pages/News/ProjectNews.vue
+++ src/pages/News/ProjectNews.vue
@@ -19,24 +19,35 @@
 				</li>
 			</ul>
 			<div class="clearfix"></div>
+			<div v-if="loadmore" class="load_more">
+				<Loading></Loading>
+			</div>
 			<!-- 版权信息 -->
 			<Copyright></Copyright>
+			<!-- 返回顶部 -->
+			<BackTop v-show="showTopBtn"></BackTop>
 		</div>
 	</div>
 </template>
 
 <script>
+	import $ from "jquery";
 	// 组件
 	import Loading from "components/Common/Loading.vue";
-    import Copyright from "components/Common/Copyright.vue";
+	import Copyright from "components/Common/Copyright.vue";
+	import BackTop from "components/Common/BackTop.vue";
 
 	export default {
 		name: "projectNews",
-		components: { Loading, Copyright },
+		components: { Loading, Copyright, BackTop },
 		data() {
 			return{
 				// 是否加载
 				loading: false,
+				// 是否加载更多
+				loadmore: false,
+				// 是否显示返回顶部按钮
+				showTopBtn: false,
 				// 资讯列表
 				newsList: [
 					{
@@ -91,6 +102,47 @@
 					}
 				]
 			}
+		},
+		created(){
+			var self = this;
+			// 监听页面滚动事件
+			$(window).scroll(function(){
+				let scrollTop = $(this).scrollTop(),
+					windowH = $(this).height(),
+					documentH = $(document).height();
+				if(scrollTop + windowH >= documentH - 40){
+					self.loadData();
+				}
+				if(scrollTop >= windowH/2){
+					self.showTopBtn = true;
+					console.log(1);
+				}
+				else{
+					self.showTopBtn = false;
+				}
+				console.log(scrollTop, windowH, documentH)
+			})
+		},
+		methods:{
+			// 加载数据
+			loadData(){
+				this.loadmore = true;
+				var self = this;
+				setTimeout(function(){
+					self.newsList.push({
+						title: '这里是新加载的内容',
+						tags: [ '申报', '通知', '项目资讯'],
+						time: '2018/12/4'
+					},
+					{
+						title: '这里是新加载的内容',
+						tags: [ '申报', '通知', '项目资讯'],
+						time: '2018/12/4'
+					});
+
+					self.loadmore = false;
+				}, 1000)
+			}
 		}
 	};
 </script>
@@ -109,6 +161,17 @@
 
 		.news_li_time{
 			.mt(5);
+		}
+	}
+
+	/* 加载更多 */
+	.load_more{
+		position: relative;
+		border-bottom: @border_deep;
+		.ht(80);
+
+		.loading{
+			position: absolute;
 		}
 	}
 </style>
